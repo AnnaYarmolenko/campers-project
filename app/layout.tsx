1ad44fbfb73a6531a0a16b1630fa7f927f07e785
AnnaYarmolenko <93723763+AnnaYarmolenko@users.noreply.github.com>
2025-12-09 23:01:37 +0200

    Fix: move font variable to html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>
+    <html lang="en" className={inter.variable}>
+      <body>
         <Header />
         <main className="container">{children}</main>
       </body>
