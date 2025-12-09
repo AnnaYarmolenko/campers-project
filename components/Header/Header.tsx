5609dc6c7b5344599a17316a0c61f2b89c5f0bfd
AnnaYarmolenko <93723763+AnnaYarmolenko@users.noreply.github.com>
2025-12-10 00:09:40 +0200

    Fix: full-width header, correct background color, and active menu highlight

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import css from "./Header.module.css";
 
 export default function Header() {
+  const pathname = usePathname();
+
+  const isHome = pathname === "/";
+  const isCatalog = pathname.startsWith("/catalog");
+
   return (
-    <header>
-      <div className={`container ${css.header}`}>
+    <header className={css.header}>
+      <div className={`container ${css.inner}`}>
         <div>
           <Link href="/">
             <svg width="136" height="16">
@@ -12,9 +20,18 @@ export default function Header() {
             </svg>
           </Link>
         </div>
+
         <nav className={css.nav}>
-          <Link href="/">Home</Link>
-          <Link href="/catalog">Catalog</Link>
+          <Link href="/" className={isHome ? css.linkActive : css.link}>
+            Home
+          </Link>
+
+          <Link
+            href="/catalog"
+            className={isCatalog ? css.linkActive : css.link}
+          >
+            Catalog
+          </Link>
         </nav>
       </div>
     </header>
